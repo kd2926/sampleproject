83e518a292a90c560cfef8e452ada3fdd1bb3ca8
kd2926 <letsdothis2926@gmail.com>
2018-06-05 20:14:08 -0700

    Added license to setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+with open(path.join(here, 'LICENSE')) as f:
+    license = f.read()
 
 setup(name='sample',
       version='0.1',
       description='Sample project for data engineer',
+      long_description = long_description,
       url='https://github.com/kd2926/sampleproject',
       author='LetsDoThis',
       author_email='letsdothis2926@gmail.com',
-      license='JN',
+      license=license,
       packages=find_packages(),
       scripts=['sample/fever_count_per_day_zip.py'],
       install_requires= [ 'pandas'],
